9b323997a1969d70821faaba84f9f3008014f41c
elamir <amirrezatm@protonmail.com>
2022-09-16 15:15:26 +0430

    files added again

--- control.sh
+++ control.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 delay=$1
-wasoffc="true"
-wasoffn="true"
 counter=0
 IDSET=''
+wasoffc="true"
+wasoffn="true"
+
 while [ true ]
 do
+
 situc=$( xset -q | sed -n 's/^.*Caps Lock:\s*\(\S*\).*$/\1/p' )
 situn=$( xset -q | sed -n 's/^.*Num Lock:\s*\(\S*\).*$/\1/p' )
+
 if [ $situc == "on" ] && [ ${wasoffc} == "true" ];
 then
-    ./notify-send.sh --close=$IDSET
-    IDSET=$(./notify-send.sh "Caps Lock" "ON" -t $delay -i /usr/share/icons/kbled-indicator/capson.png --print-id)
+    ./notifire --close=$IDSET
+    IDSET=$(./notifire "Caps Lock" "ON" -t $delay -i /usr/share/icons/kbled-indicator/capson.png -p)
     wasoffc="false"
 fi
 if [ ${wasoffc} == "false" ] && [ $situc == "off" ];
 then
-    ./notify-send.sh --close=$IDSET
-    IDSET=$(./notify-send.sh "Caps Lock" "OFF" -t $delay -i /usr/share/icons/kbled-indicator/capsoff.png --print-id)
+    ./notifire --close=$IDSET
+    IDSET=$(./notifire "Caps Lock" "OFF" -t $delay -i /usr/share/icons/kbled-indicator/capsoff.png -p)
     wasoffc="true"
 fi
 
 if [ $situn == "on" ] && [ ${wasoffn} == "true" ];
 then
-    ./notify-send.sh --close=$IDSET
-    IDSET=$(./notify-send.sh "Num Lock" "ON"  -t $delay -i /usr/share/icons/kbled-indicator/numon.png --print-id)
+    ./notifire --close=$IDSET
+    IDSET=$(./notifire "Num Lock" "ON"  -t $delay -i /usr/share/icons/kbled-indicator/numon.png -p)
     wasoffn="false"
 fi
 if [ $situn == "off" ] && [ ${wasoffn} == "false" ];
 then
-    ./notify-send.sh --close=$IDSET
-    IDSET=$(./notify-send.sh "Num Lock" "OFF"  -t $delay -i /usr/share/icons/kbled-indicator/numoff.png --print-id)
+    ./notifire --close=$IDSET
+    IDSET=$(./notifire "Num Lock" "OFF"  -t $delay -i /usr/share/icons/kbled-indicator/numoff.png -p)
     wasoffn="true"
 fi
+echo $IDSET
 sleep 0.05
 counter=$(($counter + 1))
 if [ $counter == $((delay*20)) ];
